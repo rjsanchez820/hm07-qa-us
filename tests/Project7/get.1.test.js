159d890ff65acadb0a2b410acef54eaea639ef9e
Practicum Student <rjsanchez.ins@gmail.com>
2024-09-24 18:18:09 -0700

    get.1.test.js ../../package-lock.json

--- tests/Project7/get.1.test.js
+++ tests/Project7/get.1.test.js
@@ -1,10 +1,10 @@
 const request = require('supertest');
-const apiEndpoint = require('https://cnt-b1239970-ed46-4455-a573-119728f15023.containerhub.tripleten-services.com/'; // Import API endpoint from config
+const apiEndpoint = 'https://cnt-b1239970-ed46-4455-a573-119728f15023.containerhub.tripleten-services.com/'; // Import API endpoint from config
 
 describe('GET /products', () => {
 
   // Test 1: Check the response status code
-  it('should return status code 200', async () => {
+  it('should return a list of products', async () => {
     const response = await request(apiEndpoint).get('/products'); // GET request
     expect(response.statusCode).toBe(200); // Expect 200 OK
   });
