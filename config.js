f5195b6013ee3dc69664c9cee2628457a377ea03
Practicum Student <rjsanchez.ins@gmail.com>
2024-10-02 17:36:04 -0700

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-c1f21efe-aea6-43e0-ac49-46e5bf7f2446.containerhub.tripleten-services.com//"
+    API_URL: "https://cnt-11f6fbb8-5a75-4f15-bba2-8b21f2d0a164.containerhub.tripleten-services.com/"
 }
