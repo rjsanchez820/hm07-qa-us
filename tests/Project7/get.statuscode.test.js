43ae170162a4d072744ed901ca81298697cfb79f
Practicum Student <rjsanchez.ins@gmail.com>
2024-10-02 17:52:02 -0700

    get.status.code.test.js

--- tests/Project7/get.statuscode.test.js
+++ tests/Project7/get.statuscode.test.js
@@ -1,30 +1,32 @@
+// eslint-disable-next-line no-undef
 const config = require('../config');
-
 /**
- * @api 
- * @apiName 
- * @apiGroup 
- * @apiDescription 
+ * @api {get} /your/endpoint Check Response Status Code
+ * @apiName GetStatusCode
+ * @apiGroup YourGroup
+ * @apiDescription This test checks whether the API responds with a status code of 200 for a successful GET request.
  * 
- * @apiSuccess 
+ * @apiSuccess {Number} status The HTTP status code of the response.
  * 
- * @apiSuccessExample 
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
  * 
- * @apiError 
- * @apiErrorExample 
- */ 
-
-const config = require('../config');
+ * @apiError {String} error Description of the error that occurred, if any.
+ * 
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 500 Internal Server Error
+ *     {
+ *       "error": "Internal server error"
+ *     }
+ */
 
 // Test to check the response status code for a GET request
 test('should return status 200 for the GET request', async () => {
-  let response;
-
   try {
-    response = await fetch(`${config.API_URL}/your/endpoint`);
+    const response = await fetch(`${config.API_URL}/your/endpoint`);
+
+    expect(response.status).toBe(200); // Check that the status code is 200 (OK)
   } catch (error) {
     console.error('Error during GET request:', error);
   }
-
-  expect(response?.status).toBe(200); // Check that the status code is 200 (OK)
 });
