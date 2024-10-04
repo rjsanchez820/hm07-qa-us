d38cf85e7e55715ddee929914e225264fb4ebc2b
Practicum Student <rjsanchez.ins@gmail.com>
2024-10-04 09:51:24 -0700

    get.statuscode.test.js

--- tests/Project7/get.statuscode.test.js
+++ tests/Project7/get.statuscode.test.js
@@ -1,13 +1,31 @@
 const config = require('../config');
 
-test('should return status 200 for the GET request', async () => {
+test('should return status 200 and correct product data for the GET request', async () => {
   let response;
 
   try {
-    response = await fetch(`${config.API_URL}/your/endpoint`);
+    response = await fetch(`${config.API_URL}/kits?cardId=1`);
   } catch (error) {
     console.error('Error during GET request:', error);
+    throw new Error('GET request failed'); 
   }
 
-  expect(response.status).toBe(200); // Check that the status code is 200 (OK)
+  expect(response.status).toBe(200);
+
+  const responseData = await response.json();
+
+  expect(responseData).toEqual({
+    id: 1,
+    name: "For picnic",
+    productsList: [
+      {
+        id: 1,
+        name: "Orange Juice - Cold-Pressed, No Added Sugar, Preservative Free",
+        price: 2,
+        weight: 473,
+        units: "ml",
+        quantity: 1
+      }
+    ]
+  });
 });
