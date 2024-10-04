e9b80ddb97f79c36d72cb9a6151dc3e772df3049
Practicum Student <rjsanchez.ins@gmail.com>
2024-10-04 11:51:44 -0700

    All

--- tests/Project7/get.statuscode.test.js
+++ tests/Project7/get.statuscode.test.js
@@ -10,15 +10,13 @@ test('should return status 200 and product with id 1 for the GET request', async
     throw new Error('GET request failed'); 
   }
 
-  // Expecting a 200 status for a successful request
   expect(response.status).toBe(200);
 
   const responseData = await response.json();
 
-  // Check that the response contains the product with id 1
   const productWithId1 = responseData.productsList.find(product => product.id === 1);
   
-  expect(productWithId1).toBeDefined(); // Ensure the product exists
+  expect(productWithId1).toBeDefined(); 
   expect(productWithId1).toEqual({
     id: 1,
     name: "Orange Juice - Cold-Pressed, No Added Sugar, Preservative Free",
