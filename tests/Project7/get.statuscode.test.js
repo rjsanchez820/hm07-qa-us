23135071d73fef82fdb217fcc8f0b14197a1ea43
Practicum Student <rjsanchez.ins@gmail.com>
2024-10-04 12:04:06 -0700

    All

--- tests/Project7/get.statuscode.test.js
+++ tests/Project7/get.statuscode.test.js
@@ -1,6 +1,6 @@
 const config = require('../../config');
 
-test('should return status 200 and product with id 1 for the GET request', async () => {
+test('should return status 200 for the GET request', async () => {
   let response;
 
   try {
@@ -10,19 +10,6 @@ test('should return status 200 and product with id 1 for the GET request', async
     throw new Error('GET request failed'); 
   }
 
+  // Check that the response status is 200
   expect(response.status).toBe(200);
-
-  const responseData = await response.json();
-
-  const productWithId1 = responseData.productsList.find(product => product.id === 1);
-  
-  expect(productWithId1).toBeDefined(); 
-  expect(productWithId1).toEqual({
-    id: 1,
-    name: "Orange Juice - Cold-Pressed, No Added Sugar, Preservative Free",
-    price: 2,
-    weight: 473,
-    units: "ml",
-    quantity: 1
-  });
 });
