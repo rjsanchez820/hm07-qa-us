b4726fa77654e39073f3bcce6c66b37f80d7f0f4
Practicum Student <rjsanchez.ins@gmail.com>
2024-10-04 18:10:51 -0700

    All

---
+++ tests/getHandlers.test.js
@@ -0,0 +1,9 @@
+const config = require('../config');
+
+test('', async () => {
+	try {
+		const response = await fetch(`${config.API_URL}/your/endpoint`);
+	} catch (error) {
+		console.error(error);
+	}
+});
